1afa3611bac71d27fb6a58616b73477dc2767b2f
priyanka <priyanka@priyankas-MacBook-Pro.local>
2022-06-28 16:16:22 +0530

    Added landing page. And login page.
    Co-authored By Saranya<saranyaeswaralal@gmail.com>

--- src/main/java/com/tw/vapsi/biblioteca/BibliotecaApplication.java
+++ src/main/java/com/tw/vapsi/biblioteca/BibliotecaApplication.java
@@ -1,7 +1,11 @@
 package com.tw.vapsi.biblioteca;
 
+import com.tw.vapsi.biblioteca.model.Book;
+import com.tw.vapsi.biblioteca.repository.BooksRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BibliotecaApplication {
@@ -10,4 +14,14 @@ public class BibliotecaApplication {
 		SpringApplication.run(BibliotecaApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner setUp(BooksRepository repository) {
+		return (args) -> {
+			Book book1 = new Book("Northanger Abbey", "Austen, Jane","General",1, true,1814);
+			Book book2 = new Book("War and Peace", "Tolstoy, Leo", "General",1, true,1865);
+			repository.save(book1);
+			repository.save(book2);
+
+		};
+	}
 }
--- src/main/java/com/tw/vapsi/biblioteca/controller/HomeController.java
+++ src/main/java/com/tw/vapsi/biblioteca/controller/HomeController.java
@@ -1,14 +1,17 @@
 package com.tw.vapsi.biblioteca.controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String base() {
-        return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    public String base(Model model) {
+        model.addAttribute("welcomeMessage","Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+        return "index";
     }
 }
