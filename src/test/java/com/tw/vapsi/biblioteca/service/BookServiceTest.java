5656fecc4c482ed41294e2702f21100163829af9
Nidhi Yadav <nidhiyadav@Nidhis-MacBook-Pro.local>
2022-06-29 05:54:50 +0530

    Add tasks for test cases in config.yml

--- src/test/java/com/tw/vapsi/biblioteca/service/BookServiceTest.java
+++ src/test/java/com/tw/vapsi/biblioteca/service/BookServiceTest.java
@@ -2,7 +2,6 @@ package com.tw.vapsi.biblioteca.service;
 
 import com.tw.vapsi.biblioteca.model.Book;
 import com.tw.vapsi.biblioteca.repository.BooksRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,7 +10,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
@@ -22,21 +21,17 @@ class BookServiceTest {
     @MockBean
     private BooksRepository booksRepository;
 
-    @BeforeEach
-    void setUp () {
-
-    }
 
     @Test
-    void shouldReturnListOfBooks() throws Exception {
-        List<Book> listOfBooks = Arrays.asList(
-                new Book("War and Peace", "Tolstoy, Leo", "General",1, true,1865),
-                new Book("Northanger Abbey", "Austen, Jane","General",1, true,1814));
-        when(booksRepository.findAll()).thenReturn(listOfBooks);
+    void shouldReturnListOfBooks () throws Exception {
+        List<Book> listOfBooks = Arrays.asList (
+                new Book ("War and Peace", "Tolstoy, Leo", "General", 1, true, 1865),
+                new Book ("Northanger Abbey", "Austen, Jane", "General", 1, true, 1814));
+        when (booksRepository.findAll ()).thenReturn (listOfBooks);
 
-        List<Book> books = bookService.getBooks();
+        List<Book> books = bookService.getBooks ();
 
-        verify(booksRepository,times(1)).findAll();
-        assertEquals(listOfBooks,books);
+        verify (booksRepository, times (1)).findAll ();
+        assertEquals (listOfBooks, books);
     }
 }
